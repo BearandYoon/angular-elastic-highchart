b8fa24f4ab42dc5e585ada661f564390cb65d543
mac <luckysevenbear@gmail.com>
2017-01-12 11:54:11 +0200

    small fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,7 +22,7 @@ angular.module('testApp')
       });
 
       client.ping({
-        requestTimeout: 30000,
+        requestTimeout: 30000
       }, function (error) {
         if (error) {
           console.error('elasticsearch cluster is down!');
