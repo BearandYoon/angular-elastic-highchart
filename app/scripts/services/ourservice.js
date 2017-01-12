de1f9ec2f6b705f0bcd3e1f190c763b53e0b4abb
mac <luckysevenbear@gmail.com>
2017-01-12 16:27:35 +0200

    small fix

--- app/scripts/services/ourservice.js
+++ app/scripts/services/ourservice.js
@@ -9,22 +9,9 @@
  */
 angular.module('testApp')
   .service('OurService', function ($http, $window, esFactory) {
-
-    var baseURL = 'http://apilayer.net/api/timeframe';
-    var access_key = '1806e552110e2a0db06c389610afc5a5';
-    var start_date = '2016-01-01';
-    var end_date = '2016-12-31';
-    var currencies = 'USD, EUR';
-
-    function getData() {
       return esFactory({
-        host: 'localhost:9200',
+        // host: '91.121.142.195:9200',
+        host: '127.0.0.1:9200',
         log: 'trace'
-        // _index: "currency_eur"
       });
-    };
-
-    return ({
-      getData: getData
-    });
   });
